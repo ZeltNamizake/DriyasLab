ed1dece3f44e2e2b20a5162af2da41301bf97e22
ZeltNamizake <dandriyasyahdi2006@gmail.com>
2025-11-07 20:24:05 +0700

    Adding code snippets (Node.js)

--- snippets/nodejs/keypress.js
+++ snippets/nodejs/keypress.js
@@ -8,22 +8,20 @@ readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 process.stdin.resume();
 
-
 // Listen to keypress events
 const onKey = (str, key) => {
- if(key && key.name === "q") {
-  console.log("Exit...")
-  cleanup()
-  process.exit(0)
- }
- console.log(key)
-}
-process.stdin.on("keypress", onKey)
+  if (key && key.name === "q") {
+    console.log("Exit...");
+    cleanup();
+    process.exit(0);
+  }
+  console.log(key);
+};
+process.stdin.on("keypress", onKey);
 
 // Function to clear input mode
 function cleanup() {
- // stdin is not in raw mode and is inactive
+  // stdin is not in raw mode and is inactive
   if (process.stdin.isTTY) process.stdin.setRawMode(false);
   process.stdin.pause();
 }
-
